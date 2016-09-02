20a3685593b5438aaae236519ecdf7869432f638
Jordi <jordi.nopa@gmail.com>
2016-09-03 01:13:05 +0200

    Basic webpack config that generates an html

---
+++ app/main.js
@@ -0,0 +1,4 @@
+//var template = require('./templates/index.html.twig');
+
+//var html = template({name: 'World'});
+console.log('bon dia');
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require("path");
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.twig$/,
+        loader: 'twig'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file'
+      },
+      {
+        test: /\.png$/,
+        loader: 'file?mimetype=image/png'
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: 'app/templates/index.html.twig'
+    })
+  ],
+  node: {
+    fs: 'empty' //avoids error messages (at least this says twig-loader docs
+  }
+};
